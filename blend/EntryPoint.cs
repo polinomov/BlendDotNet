98dc950e51910444bcdd5c1674f48347fc25b7da
polinomov <marina_plotskaka@yahoo.com>
2021-09-13 13:43:08 -0700

    args

--- blend/EntryPoint.cs
+++ blend/EntryPoint.cs
@@ -12,9 +12,11 @@ namespace blend
         {
             Console.WriteLine("Blend");
             List<string> dllList = new List<string>();
-            dllList.Add(@"C:\Dev\BlendDotNet\libs\libb\bin\Debug\net472\libb.dll");
-            string prime  = @"C:\Dev\BlendDotNet\libs\liba\bin\Debug\net472\liba.dll";
-            string result = @"C:\Dev\BlendDotNet\result\liba.dll";
+            //dllList.Add(@"C:\Dev\BlendDotNet\libs\libb\bin\Debug\net472\libb.dll");
+            //string prime  = @"C:\Dev\BlendDotNet\libs\liba\bin\Debug\net472\liba.dll";
+            string prime = args[0];
+            dllList.Add(args[1]);
+            string result = args[2];
             try
             {
                 Mixer.DoMix(prime, dllList, result);
